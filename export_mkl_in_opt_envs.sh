5b82960281945fa8f28e0f471dd3d7582b5fc7d8
basRizk <basem.rizk@outlook.com>
2019-06-05 15:43:34 +0200

    Updating envs and alternatives files

---
+++ export_mkl_in_opt_envs.sh
@@ -0,0 +1,9 @@
+export LD_LIBRARY_PATH=/opt/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin:/opt/intel/compilers_and_libraries_2019.4.243/linux/compiler/lib/intel64_lin:/opt/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin 
+
+export CMAKE_LIBARY_PATH=/opt/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin:/opt/intel/compilers_and_libraries_2019.4.243/linux/compiler/lib/intel64_lin:/opt/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin
+
+export MKLROOT=/opt/intel/mkl \
+
+export MKL_ROOT=/opt/intel/mkl \
+
+echo "Exporting based that INTEL MKL is in OPT directory."
--- exporting_env_vars.sh
+++
@@ -1,9 +0,0 @@
-export LD_LIBRARY_PATH=/home/ironbas7/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin:/home/ironbas7/intel/compilers_and_libraries_2019.4.243/linux/compiler/lib/intel64_lin:/home/ironbas7/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin 
-
-export CMAKE_LIBARY_PATH=/home/ironbas7/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin:/home/ironbas7/intel/compilers_and_libraries_2019.4.243/linux/compiler/lib/intel64_lin:/home/ironbas7/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin
-
-export MKLROOT=/home/$USER/intel/mkl \
-
-export MKL_ROOT=/home/$USER/intel/mkl \
-
-echo "Exporting based that INTEL MKL is in HOME directory."
