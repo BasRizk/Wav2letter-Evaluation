e529e3c040d07bdbf8e36e9cc0bf9a838f5a1b9d
basRizk <basem.rizk@outlook.com>
2019-08-09 18:50:49 +0200

    fix create path after check

--- prepare_en_test.sh
+++ prepare_en_test.sh
@@ -4,8 +4,8 @@
 W2L_TEST_DIR=tests
 W2L_EN_TEST_DIR=$W2L_TEST_DIR/en-data
 
-mkdir -p $W2L_EN_TEST_DIR
 if [ ! -d $W2L_EN_TEST_DIR ]; then
+  mkdir -p $W2L_EN_TEST_DIR
   wget -qO- http://www.openslr.org/resources/12/test-other.tar.gz | tar xvz -C $W2L_TEST_DIR
 fi
 
