17df4e2837c31cd1353914d0fc95d7ec8052b04c
ironbas3 <basem.rizk@outlook.com>
2019-08-12 16:15:12 +0200

    eval on i7H + fully automated test

--- run_full_test.sh
+++ run_full_test.sh
@@ -4,6 +4,6 @@ then
   echo "Export TEST_DIR first. example 'en-data/iisys' Or 'en-data/test-other' "
   echo "Export USE_GPU='ON' or 'OFF'"
 else
-  Test --flagsfile=test_en.cfg --test=$TEST_DIR && Decoder --flagsfile=decode_en.cfg --test=$TEST_DIR && python create_benchmark.py $TEST_DIR
+  Test --flagsfile=test_en.cfg --test=$TEST_DIR && Decoder --flagsfile=decode_en.cfg --test=$TEST_DIR && python create_benchmark.py $TEST_DIR $USE_GPU
 
 fi
